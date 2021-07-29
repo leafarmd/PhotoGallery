a15d5410c833f3fddb812f7ec1ef328e8ec27b99
Rafael Damasceno <rafael.damasceno@realizecfi.com.br>
2021-07-29 14:00:46 -0300

    create xcode project and initial setup

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Rafael Mendes Damasceno on 29/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
