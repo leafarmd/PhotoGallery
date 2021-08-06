fc9726c10639a04c2707a6c3031c4cc0d2238835
Rafael Damasceno <rafael.damasceno@realizecfi.com.br>
2021-08-06 08:32:21 -0300

    add image gallery presenter tests

---
+++ PhotoGalleryTests/PGImageGalleryPresenterTests.swift
@@ -0,0 +1,14 @@
+//
+//  PGPhotoGalleryPresenterTests.swift
+//  PhotoGalleryTests
+//
+//  Created by Rafael Mendes Damasceno on 06/08/21.
+//
+
+import XCTest
+@testable import PhotoGallery
+
+class PGPhotoGalleryPresenterTests: XCTestCase {
+    
+    var sut: PGPImageGalleryPresenter?
+}
